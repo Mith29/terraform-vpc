a35bc4c03327b22538933a44c578c2431bc23cd8
Mith29 <mithra29.venu@gmail.com>
2024-06-26 15:18:56 -0400

    Update 10-key.tf

--- 10-key.tf
+++ 10-key.tf
@@ -5,7 +5,7 @@
 
 resource "aws_key_pair" "key" {
   key_name   = "${var.project}-key"
-  public_key = file("/root/.ssh/id_rsa.pub")
+  public_key = file("/root/.ssh/id_ed25519.pub")
   tags = {
     Name = "${var.project}-key"
     Project = var.project
